f1b77c8a8ecb3b4e4a893aa0e83851975ab66d77
kamillysy <kamillysy@interia.pl>
2016-02-18 19:48:03 +0100

    modified:   PlatformController/app/src/main/java/com/example/kamillysy/platformcontroller/PlatformController.java 	README.txt

--- PlatformController/app/src/main/java/com/example/kamillysy/platformcontroller/PlatformController.java
+++ PlatformController/app/src/main/java/com/example/kamillysy/platformcontroller/PlatformController.java
@@ -1,13 +1,9 @@
 package com.example.kamillysy.platformcontroller;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.bluetooth.*;
 import android.content.DialogInterface;
-import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
-import android.net.Uri;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -17,9 +13,6 @@ import android.widget.SeekBar;
 import android.widget.Toast;
 import android.view.View.OnTouchListener;
 import android.view.View;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import java.util.Set;
 import java.util.ArrayList;
 import android.widget.TextView;
@@ -32,40 +25,38 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 public class PlatformController extends AppCompatActivity {
     BluetoothAdapter btAdapter;
     Button btlist;
-    ImageButton imageButtons[]=new ImageButton[8];
+    ImageButton imageButtons[] = new ImageButton[8];
     Set<BluetoothDevice> pairedDevices;
     BluetoothSocket socket = null;
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     SeekBar seekBar;
     TextView textSpeed;
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_platform_controller);
-        BTinit();
-        buttonsiniter();
-        PWMcontrol();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        btIniter();
+        buttonsIniter();
+        pwmControl();
     }
 
-    private void BTinit() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(getApplicationContext(), "Bluetooth must be enabled to continue", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-
-        btAdapter=BluetoothAdapter.getDefaultAdapter();
-        if(btAdapter==null) {
+    private void btIniter() {
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter == null) {
             Toast.makeText(getApplicationContext(), "No bluetooth detected", Toast.LENGTH_LONG).show();
             finish();
         }
-        else{
+        else {
             if (!btAdapter.isEnabled()) {
                 Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(intent, 1);
@@ -73,87 +64,29 @@ public class PlatformController extends AppCompatActivity {
 
         }
     }
-    private void PWMcontrol(){
-        seekBar=(SeekBar)findViewById(R.id.seekBar);
-        textSpeed=(TextView)findViewById(R.id.textView);
-        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
-            int progress=0;
-            @Override
-        public void onProgressChanged(SeekBar seekBar,int progressValue,boolean fromuser){
-                progress=progressValue;
-                textSpeed.setText("Speed: "+progress+"%");
-            }
-            @Override
 
-            public void onStartTrackingTouch(SeekBar seekBar) {  }
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                sendcomand(progress);
-                if(socket==null)
-                {
-                    seekBar.setProgress(50);
-                }
-            }
-        });
-    }
-
-    private void buttonsiniter(){
-         btlist=(Button)findViewById(R.id.ButtonList);
-        final int []idlist={R.id.ButtonF,R.id.ButtonLF,R.id.ButtonRF,
-                R.id.ButtonL,R.id.ButtonR,
-                R.id.ButtonB, R.id.ButtonLB,R.id.ButtonRB};
-        final int []commands={111,114,115,140,150,122,124,125};
-        for(int it=0;it<imageButtons.length;it++){
-            imageButtons[it]=(ImageButton)findViewById(idlist[it]);
-            final int finalIt = it;
-            imageButtons[finalIt].setOnTouchListener(new OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    buttons(event,commands[finalIt]);
-                    return false;
-                }
-            });
-        }
-
-
-    }
-
-    private void buttons(MotionEvent event,int command){
-        if(event.getAction()==MotionEvent.ACTION_DOWN){
-            sendcomand(command);
-        }
-        else if(event.getAction()==MotionEvent.ACTION_UP){
-            sendcomand(160);
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_CANCELED){
-            Toast.makeText(getApplicationContext(), "Bluetooth must be enabled to continue", Toast.LENGTH_SHORT).show();
-        }
-    }
 
     public void btList(View view) {
-        pairedDevices=btAdapter.getBondedDevices();
-        final ArrayList list= new ArrayList();
-        final ArrayList addresses=new ArrayList();
+        pairedDevices = btAdapter.getBondedDevices();
+        final ArrayList list = new ArrayList();
+        final ArrayList addresses = new ArrayList();
 
-        CharSequence[] ls= new CharSequence[pairedDevices.size()];
+        CharSequence[] ls = new CharSequence[pairedDevices.size()];
 
-        if(pairedDevices.size()>0){
-             int it=0;
-            for(BluetoothDevice bt: pairedDevices){
+        if (pairedDevices.size() > 0) {
+            int it = 0;
+            for (BluetoothDevice bt : pairedDevices) {
                 addresses.add(bt.getAddress());
-                list.add(bt.getName()+"\n" + bt.getAddress());
-                ls[it]=list.get(it).toString();
+                list.add(bt.getName() + "\n" + bt.getAddress());
+                ls[it] = list.get(it).toString();
                 it++;
             }
         }
-        else{
-            Toast.makeText(getApplicationContext(),"No Paired Bluetooth Devices",Toast.LENGTH_SHORT).show();
+        else {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices", Toast.LENGTH_SHORT).show();
         }
 
-        AlertDialog.Builder builder=new AlertDialog.Builder(this,AlertDialog.THEME_DEVICE_DEFAULT_DARK);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, AlertDialog.THEME_DEVICE_DEFAULT_DARK);
         builder.setTitle("Choose paired BT device");
         builder.setItems(ls, new DialogInterface.OnClickListener() {
             //@Override
@@ -165,7 +98,8 @@ public class PlatformController extends AppCompatActivity {
         builder.create();
         builder.show();
     }
-    private void connect(String deviceadress ){
+
+    private void connect(String deviceadress) {
         final String adress = deviceadress;
         BluetoothDevice platform = btAdapter.getRemoteDevice(adress);
         try {
@@ -174,72 +108,84 @@ public class PlatformController extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(socket.isConnected()){
+        if (socket.isConnected()) {
             btlist.setText("Connected");
-        }
-        else{
-            socket=null;
+        } else {
+            socket = null;
             btlist.setText("Error, check the platform!");
         }
 
     }
 
-    private void sendcomand(int x){
-        if(socket!=null) {
+    private void buttonsIniter() {
+        btlist = (Button) findViewById(R.id.ButtonList);
+        final int[] idlist = {R.id.ButtonF, R.id.ButtonLF, R.id.ButtonRF,
+                R.id.ButtonL, R.id.ButtonR,
+                R.id.ButtonB, R.id.ButtonLB, R.id.ButtonRB};
+        final int[] commands = {111, 114, 115, 140, 150, 122, 124, 125};
+
+        for (int it = 0; it < imageButtons.length; it++) {
+            imageButtons[it] = (ImageButton) findViewById(idlist[it]);
+            final int finalIt = it;
+            imageButtons[finalIt].setOnTouchListener(new OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    buttonsAction(event, commands[finalIt]);
+                    return false;
+                }
+            });
+        }
+    }
+
+    private void buttonsAction(MotionEvent event, int command) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            sendComand(command);
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            sendComand(160);
+        }
+    }
+
+    private void pwmControl() {
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        textSpeed = (TextView) findViewById(R.id.textView);
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromuser) {
+                progress = progressValue;
+                textSpeed.setText("Speed: " + progress + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                sendComand(progress);
+                if (socket == null) {
+                    seekBar.setProgress(50);
+                }
+            }
+        });
+    }
+
+
+
+    private void sendComand(int x) {
+        if (socket != null) {
             try {
                 socket.getOutputStream().write((byte) x);
             } catch (IOException e) {
                 e.printStackTrace();
-                    socket=null;
-                    btlist.setText("Error, check the connection!");
+                socket = null;
+                btlist.setText("Error, check the connection!");
             }
+        } else {
+            Toast.makeText(getApplicationContext(), "First, connect to the platform", Toast.LENGTH_SHORT).show();
         }
-        else{
-            Toast.makeText(getApplicationContext(),"First, connect to the platform",Toast.LENGTH_SHORT).show();
-        }
     }
 
-
-
-
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "PlatformController Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.kamillysy.platformcontroller/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "PlatformController Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.kamillysy.platformcontroller/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
+
